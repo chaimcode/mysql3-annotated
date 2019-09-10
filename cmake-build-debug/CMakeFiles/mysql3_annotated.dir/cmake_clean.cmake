5a7ee742be49586f126de04807eb135dabe131ca
cm <cm@lizhiweike.com>
2019-09-10 15:59:28 +0800

    init project

---
+++ cmake-build-debug/CMakeFiles/mysql3_annotated.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mysql3_annotated.dir/main.c.o"
+  "mysql3_annotated.pdb"
+  "mysql3_annotated.exe"
+  "libmysql3_annotated.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mysql3_annotated.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
